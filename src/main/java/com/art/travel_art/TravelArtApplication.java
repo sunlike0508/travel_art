55cba0cb5ba7c6613f534b2447b982929e9a7130
seonhoshin <sunlike0508@waug.com>
2024-08-22 23:31:47 +0900

    초기화

---
+++ src/main/java/com/art/travel_art/TravelArtApplication.java
@@ -0,0 +1,13 @@
+package com.art.travel_art;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelArtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelArtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/art/travel_art/TravelArtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.art.travel_art;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelArtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
